faf87a1ecf50499a83c20ce3ab167fa853ca7819
Fatma <fatma.abdullayewa@gmail.com>
2026-01-11 02:08:34 +0300

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+
 const nextConfig = {
    images: {
     domains: ["res.cloudinary.com"], 
-  },
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  webpack(config) {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
   },
 };
 
