7652e8931798f23b3f919f52eab1809d687fa768
Fatma <fatma.abdullayewa@gmail.com>
2026-01-11 02:48:56 +0300

    cn

--- Components/ui/alert-dialog.jsx
+++ Components/ui/alert-dialog.jsx
@@ -3,8 +3,8 @@
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
-import { cn } from "@/lib/utils/index"
-import { buttonVariants } from "@/components/ui/Button"
+import { cn } from "../../lib/utils/index"
+import { buttonVariants } from "./button"
 
 function AlertDialog({
   ...props
--- Components/ui/form.jsx
+++ Components/ui/form.jsx
@@ -8,7 +8,7 @@ import {
   useFormState,
 } from "react-hook-form";
 
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils/index";
 import { Label } from "./label";
 
 const Form = FormProvider;
